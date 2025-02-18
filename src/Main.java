584a17947168cfb026a76979862f62d3c493aa1b
gabriel.rodrigues <fgrodrigues.gabriel@gmail.com>
2025-02-18 09:15:26 -0300

    Create(calculadora)

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Resultado: " + soma(5,3));
+    }
+
+    public static int soma(int a, int b){
+        return a + b;
+    }
+}
